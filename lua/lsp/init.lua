3b894a772ce30670d0449bd48ffb3c0f662d4192
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-05-27 21:02:10 +0700

    rm legacy duplicated block

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,29 +1,3 @@
-require("mason-lspconfig").setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({})
-  end,
-})
-
-require("lspconfig").lua_ls.setup {}
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
-
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-vim.lsp.handlers["textDocument/hover"] =
-    vim.lsp.with(vim.lsp.handlers.hover, { border = require("utils").border() })
-
-vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, { border = require("utils").border() })
-
 local M = {}
 
 -- LSP servers to install
