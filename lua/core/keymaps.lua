e084ac05b3721c54636416724e1f525e4140d684
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-05-27 20:30:11 +0700

    refactor key mappings to use explicit options for noremap and silent, enhancing consistency and clarity

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,67 +5,67 @@ vim.api.nvim_create_autocmd("User", {
     local opts = { noremap = true, silent = true }
 
     -- Better window navigation
-    map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", ...opts })
-    map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", ...opts })
-    map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", ...opts })
-    map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", ...opts })
+    map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", noremap = true, silent = true })
+    map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", noremap = true, silent = true })
+    map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true, silent = true })
+    map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", noremap = true, silent = true })
 
     -- Resize windows
-    map("n", "<C-Up>", ":resize -2<CR>", { desc = "Resize window up", ...opts })
-    map("n", "<C-Down>", ":resize +2<CR>", { desc = "Resize window down", ...opts })
-    map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize window left", ...opts })
-    map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize window right", ...opts })
+    map("n", "<C-Up>", ":resize -2<CR>", { desc = "Resize window up", noremap = true, silent = true })
+    map("n", "<C-Down>", ":resize +2<CR>", { desc = "Resize window down", noremap = true, silent = true })
+    map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize window left", noremap = true, silent = true })
+    map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize window right", noremap = true, silent = true })
 
     -- Better buffer navigation
-    map("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer", ...opts })
-    map("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer", ...opts })
-    map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer", ...opts })
+    map("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer", noremap = true, silent = true })
+    map("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer", noremap = true, silent = true })
+    map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer", noremap = true, silent = true })
 
     -- Telescope
-    map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files" })
-    map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live Grep" })
-    map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find Buffers" })
-    map("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Help Tags" })
-    map("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Recent Files" })
+    map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files", noremap = true, silent = true })
+    map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live Grep", noremap = true, silent = true })
+    map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find Buffers", noremap = true, silent = true })
+    map("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Help Tags", noremap = true, silent = true })
+    map("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Recent Files", noremap = true, silent = true })
 
     -- NvimTree
-    map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
-    map("n", "<leader>o", ":NvimTreeFocus<CR>", { desc = "Focus File Explorer" })
+    map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer", noremap = true, silent = true })
+    map("n", "<leader>o", ":NvimTreeFocus<CR>", { desc = "Focus File Explorer", noremap = true, silent = true })
 
     -- LSP
-    map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-    map("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
-    map("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
-    map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-    map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
-    map("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type Definition" })
-    map("n", "<leader>ds", vim.lsp.buf.document_symbol, { desc = "Document Symbols" })
-    map("n", "<leader>ws", vim.lsp.buf.workspace_symbol, { desc = "Workspace Symbols" })
+    map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition", noremap = true, silent = true })
+    map("n", "gr", vim.lsp.buf.references, { desc = "Go to References", noremap = true, silent = true })
+    map("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation", noremap = true, silent = true })
+    map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", noremap = true, silent = true })
+    map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol", noremap = true, silent = true })
+    map("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type Definition", noremap = true, silent = true })
+    map("n", "<leader>ds", vim.lsp.buf.document_symbol, { desc = "Document Symbols", noremap = true, silent = true })
+    map("n", "<leader>ws", vim.lsp.buf.workspace_symbol, { desc = "Workspace Symbols", noremap = true, silent = true })
 
     -- LLM.nvim
-    map("n", "<leader>ll", ":Llm<CR>", { desc = "Open LLM Chat" })
-    map("n", "<leader>lc", ":LlmContext<CR>", { desc = "Open LLM Context" })
-    map("v", "<leader>ll", ":Llm<CR>", { desc = "Open LLM Chat with Selection" })
-    map("v", "<leader>lc", ":LlmContext<CR>", { desc = "Open LLM Context with Selection" })
+    map("n", "<leader>ll", ":Llm<CR>", { desc = "Open LLM Chat", noremap = true, silent = true })
+    map("n", "<leader>lc", ":LlmContext<CR>", { desc = "Open LLM Context", noremap = true, silent = true })
+    map("v", "<leader>ll", ":Llm<CR>", { desc = "Open LLM Chat with Selection", noremap = true, silent = true })
+    map("v", "<leader>lc", ":LlmContext<CR>", { desc = "Open LLM Context with Selection", noremap = true, silent = true })
 
     -- Git
-    map("n", "<leader>gg", ":LazyGit<CR>", { desc = "LazyGit" })
-    map("n", "<leader>gb", ":GitBlameToggle<CR>", { desc = "Toggle Git Blame" })
+    map("n", "<leader>gg", ":LazyGit<CR>", { desc = "LazyGit", noremap = true, silent = true })
+    map("n", "<leader>gb", ":GitBlameToggle<CR>", { desc = "Toggle Git Blame", noremap = true, silent = true })
 
     -- Terminal
-    map("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle Terminal" })
-    map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit Terminal Mode" })
+    map("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle Terminal", noremap = true, silent = true })
+    map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit Terminal Mode", noremap = true, silent = true })
 
     -- Quickfix
-    map("n", "<leader>co", ":copen<CR>", { desc = "Open Quickfix" })
-    map("n", "<leader>cc", ":cclose<CR>", { desc = "Close Quickfix" })
-    map("n", "<leader>cn", ":cnext<CR>", { desc = "Next Quickfix Item" })
-    map("n", "<leader>cp", ":cprev<CR>", { desc = "Previous Quickfix Item" })
+    map("n", "<leader>co", ":copen<CR>", { desc = "Open Quickfix", noremap = true, silent = true })
+    map("n", "<leader>cc", ":cclose<CR>", { desc = "Close Quickfix", noremap = true, silent = true })
+    map("n", "<leader>cn", ":cnext<CR>", { desc = "Next Quickfix Item", noremap = true, silent = true })
+    map("n", "<leader>cp", ":cprev<CR>", { desc = "Previous Quickfix Item", noremap = true, silent = true })
 
     -- Misc
-    map("n", "<leader>w", ":w<CR>", { desc = "Save File" })
-    map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
-    map("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Search Highlight" })
-    map("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
+    map("n", "<leader>w", ":w<CR>", { desc = "Save File", noremap = true, silent = true })
+    map("n", "<leader>q", ":q<CR>", { desc = "Quit", noremap = true, silent = true })
+    map("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Search Highlight", noremap = true, silent = true })
+    map("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree", noremap = true, silent = true })
   end,
 })
